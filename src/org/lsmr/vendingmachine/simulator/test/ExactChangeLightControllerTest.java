42ba86fdd710cf14b1bf2aaa1bdfefcb1ee59511
ajtoyoda <ajtoyoda@ucalgary.ca>
2015-01-30 09:09:10 -0700

    Adding in sample test case

---
+++ src/org/lsmr/vendingmachine/simulator/test/ExactChangeLightControllerTest.java
@@ -0,0 +1,26 @@
+package org.lsmr.vendingmachine.simulator.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.lsmr.vendingmachine.simulator.assignment1.*;
+import org.lsmr.vendingmachine.simulator.*;
+
+public class ExactChangeLightControllerTest {
+
+	@Before
+	public void setup(){
+		
+	}
+	@After
+	public void tearDown(){
+		
+	}
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	private ExactChangeLightController exactLight;
+}
