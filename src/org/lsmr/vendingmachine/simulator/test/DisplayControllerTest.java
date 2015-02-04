8bd2f0def279aa6bb441a5f4378619ef2661106a
Sean Cox <seanmdcox@gmail.com>
2015-02-03 18:31:44 -0700

    Adding updated file for display controller tests

---
+++ src/org/lsmr/vendingmachine/simulator/test/DisplayControllerTest.java
@@ -0,0 +1,83 @@
+package org.lsmr.vendingmachine.simulator.test;
+
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.lsmr.vendingmachine.simulator.DisplaySimulator;
+import org.lsmr.vendingmachine.simulator.PopCan;
+import org.lsmr.vendingmachine.simulator.PopCanRackSimulator;
+import org.lsmr.vendingmachine.simulator.assignment1.DisplayController;
+
+public class DisplayControllerTest{
+
+	@Before
+	public void setup(){
+		display = new DisplaySimulator();
+		displayController = new DisplayController(display);
+		System.setOut(new PrintStream(outStream));
+	}
+	
+	//This will happen after each and every test case
+	@After
+	public void tearDown() {
+		System.setOut(null);
+	}
+	
+	@Test
+	public void testPopAddedMsg()
+	{
+		PopCanRackSimulator popRack = new PopCanRackSimulator(10);
+		PopCan pop = new PopCan();
+		displayController.popAdded(popRack, pop);
+		assertEquals("Notice: Pop Added Successfully\n", outStream.toString());
+	}
+	
+	@Test
+	public void testPopRemovedMsg()
+	{
+		PopCanRackSimulator popRack = new PopCanRackSimulator(10);
+		PopCan pop = new PopCan();
+		displayController.popRemoved(popRack, pop);
+		assertEquals("Notice: Dispensing\n", outStream.toString());
+	}
+	
+	/*@Test
+	public void testPopAddedMsg()
+	{
+		DisplayController displayController = new DisplayController(display);
+		PopCanRackSimulator popRack = new PopCanRackSimulator(10);
+		PopCan pop = new PopCan();
+		DispenserController dispenserControl = new DispenserController(popRack,
+				null, null, null);
+		dispenserControl.register(displayController);
+		System.setOut(new PrintStream(outStream));
+		
+		try {
+			popRack.addPop(pop);
+		} catch (CapacityExceededException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (DisabledException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertEquals(" Pop Added Successfully", outStream.toString());
+		System.setOut(null);
+		
+	}
+	
+	/*@Test
+	public void test() {
+		fail("Not yet implemented");
+	}*/
+	
+	private DisplaySimulator display;
+	private DisplayController displayController;
+	private final ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+
+}
