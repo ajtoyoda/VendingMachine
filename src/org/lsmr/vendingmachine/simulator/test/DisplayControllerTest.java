5ed631fe41574536b1f7008aaa0e71d89c3c2a1e
Sean Cox <seanmdcox@gmail.com>
2015-02-03 21:44:40 -0700

    Updated version of DisplaySimulatorTest

--- src/org/lsmr/vendingmachine/simulator/test/DisplayControllerTest.java
+++ src/org/lsmr/vendingmachine/simulator/test/DisplayControllerTest.java
@@ -8,9 +8,15 @@ import java.io.PrintStream;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.lsmr.vendingmachine.simulator.CapacityExceededException;
+import org.lsmr.vendingmachine.simulator.Coin;
+import org.lsmr.vendingmachine.simulator.CoinReceptacleSimulator;
+import org.lsmr.vendingmachine.simulator.DeliveryChuteSimulator;
+import org.lsmr.vendingmachine.simulator.DisabledException;
 import org.lsmr.vendingmachine.simulator.DisplaySimulator;
 import org.lsmr.vendingmachine.simulator.PopCan;
 import org.lsmr.vendingmachine.simulator.PopCanRackSimulator;
+import org.lsmr.vendingmachine.simulator.assignment1.DispenserController;
 import org.lsmr.vendingmachine.simulator.assignment1.DisplayController;
 import org.lsmr.vendingmachine.simulator.assignment1.HardwareSimulatorExecutable;
 
@@ -20,8 +26,10 @@ public class DisplayControllerTest{
 	public void setup(){
 		display = new DisplaySimulator();
 		displayController = new DisplayController(display);
-		outStream = new ByteArrayOutputStream();
-		HardwareSimulatorExecutable.out = new PrintStream(outStream);
+		basicOutStream = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(basicOutStream));
+		hardwareOutStream = new ByteArrayOutputStream();
+		HardwareSimulatorExecutable.out = new PrintStream(hardwareOutStream);
 	}
 	
 	//This will happen after each and every test case
@@ -35,7 +43,8 @@ public class DisplayControllerTest{
 		PopCanRackSimulator popRack = new PopCanRackSimulator(10);
 		PopCan pop = new PopCan();
 		displayController.popAdded(popRack, pop);
-		assertEquals("Notice: Pop Added Successfully", outStream.toString());
+		//assertEquals("Notice: Pop Added Successfully\n", basicOutStream.toString());
+		assertEquals("Notice: Pop Added Successfully", hardwareOutStream.toString());
 	}
 	
 	@Test
@@ -44,22 +53,35 @@ public class DisplayControllerTest{
 		PopCanRackSimulator popRack = new PopCanRackSimulator(10);
 		PopCan pop = new PopCan();
 		displayController.popRemoved(popRack, pop);
-		assertEquals("Notice: Dispensing", outStream.toString());
+		//assertEquals("Notice: Dispensing", basicOutStream.toString());
+		assertEquals("Notice: Dispensing", hardwareOutStream.toString());
 	}
 	
-	/*@Test
-	public void testPopAddedMsg()
+	@Test
+	public void testPopFullMsg()
 	{
-		DisplayController displayController = new DisplayController(display);
 		PopCanRackSimulator popRack = new PopCanRackSimulator(10);
-		PopCan pop = new PopCan();
-		DispenserController dispenserControl = new DispenserController(popRack,
-				null, null, null);
-		dispenserControl.register(displayController);
-		System.setOut(new PrintStream(outStream));
-		
+		displayController.popFull(popRack);
+		//assertEquals("Notice: Pop Rack Full", basicOutStream.toString());
+		assertEquals("Notice: Pop Rack Full", hardwareOutStream.toString());
+	}
+	
+	@Test
+	public void testPopEmptyMsg()
+	{
+		PopCanRackSimulator popRack = new PopCanRackSimulator(10);
+		displayController.popEmpty(popRack);
+		//assertEquals("Notice: Pop Rack Empty", basicOutStream.toString());
+		assertEquals("Notice: Pop Rack Empty", hardwareOutStream.toString());
+	}
+	
+	@Test
+	public void testCoinAddedMsgOneCent()
+	{
+		CoinReceptacleSimulator receptacle = new CoinReceptacleSimulator(10);
+		Coin coin = new Coin(1);
 		try {
-			popRack.addPop(pop);
+			receptacle.acceptCoin(coin);
 		} catch (CapacityExceededException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -67,18 +89,147 @@ public class DisplayControllerTest{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		assertEquals(" Pop Added Successfully", outStream.toString());
-		System.setOut(null);
-		
+		displayController.coinAdded(receptacle, coin);
+		//assertEquals("$0.01", basicOutStream.toString());
+		assertEquals("$0.01", hardwareOutStream.toString());
 	}
 	
-	/*@Test
-	public void test() {
-		fail("Not yet implemented");
-	}*/
+	@Test
+	public void testCoinAddedMsgTenCent()
+	{
+		CoinReceptacleSimulator receptacle = new CoinReceptacleSimulator(10);
+		Coin coin = new Coin(10);
+		try {
+			receptacle.acceptCoin(coin);
+		} catch (CapacityExceededException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (DisabledException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		displayController.coinAdded(receptacle, coin);
+		//assertEquals("$0.10", basicOutStream.toString());
+		assertEquals("$0.10", hardwareOutStream.toString());
+	}
+	
+	@Test
+	public void testCoinAddedMsgDollar()
+	{
+		CoinReceptacleSimulator receptacle = new CoinReceptacleSimulator(10);
+		Coin coin = new Coin(100);
+		try {
+			receptacle.acceptCoin(coin);
+		} catch (CapacityExceededException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (DisabledException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		displayController.coinAdded(receptacle, coin);
+		//assertEquals("$1.00", basicOutStream.toString());
+		assertEquals("$1.00", hardwareOutStream.toString());
+	}
+
+	@Test
+	public void testMessageChangeBasic()
+	{
+		displayController.messageChange(display, "$0.00", "$1.00");
+		//assertEquals("$1.00", basicOutStream.toString());
+		assertEquals("", hardwareOutStream.toString());
+	}
+	
+	@Test
+	public void testMessageChangeNEM()
+	{
+		displayController.messageChange(display, "$0.00", "Notice: Not Enough Money");
+		//assertEquals("Notice: Not Enough Money", basicOutStream.toString());
+		assertEquals("", hardwareOutStream.toString());
+	}
+	
+	@Test
+	public void testCoinsRemovedMsg()
+	{
+		CoinReceptacleSimulator receptacle = new CoinReceptacleSimulator(10);
+		displayController.coinsRemoved(receptacle);
+		//assertEquals("$0.00", basicOutStream.toString());
+		assertEquals("$0.00", hardwareOutStream.toString());
+	}
+	
+	@Test
+	public void testCoinsFullMsg()
+	{
+		CoinReceptacleSimulator receptacle = new CoinReceptacleSimulator(10);
+		displayController.coinsFull(receptacle);
+		//assertEquals("Notice: Cannot accept anymore coins", basicOutStream.toString());
+		assertEquals("Notice: Cannot accept anymore coins", hardwareOutStream.toString());
+	}
+	
+	@Test
+	public void testEnabled()
+	{
+		//does nothing
+	}
+	
+	@Test
+	public void testDisabled()
+	{
+		//does nothing
+	}
+	
+	@Test
+	public void testNotEnoughMoneyEvent()
+	{
+		DispenserController control = new DispenserController(null, null, null, null);
+		Integer cost = 125; 
+		displayController.notEnoughMoneyEvent(control, cost);
+		//assertEquals("Notice: Not Enough Money. Selection Cost is: $1.25", basicOutStream.toString());
+		assertEquals("Notice: Not Enough Money. Selection Cost is: $1.25", hardwareOutStream.toString());
+	}
+	
+	@Test
+	public void testNotExactChange()
+	{
+		DispenserController control = new DispenserController(null, null, null, null);
+		displayController.notExactChange(control);
+		//assertEquals("Notice: Cannot Make Exact Change", basicOutStream.toString());
+		assertEquals("Notice: Cannot Make Exact Change", hardwareOutStream.toString());
+	}
+	
+	@Test
+	public void testItemDelivered()
+	{
+		DeliveryChuteSimulator chute = new DeliveryChuteSimulator(10);
+		displayController.itemDelivered(chute);
+		//assertEquals("Notice: Recieved " + chute.toString(), basicOutStream.toString());
+		assertEquals("Notice: Recieved " + chute.toString(), hardwareOutStream.toString());
+	}
+	
+	@Test
+	public void testDoorOpened()
+	{
+		//does nothing
+	}
+	
+	@Test
+	public void testDoorClosed()
+	{
+		//does nothing
+	}
+	
+	@Test
+	public void testChuteFull()
+	{
+		DeliveryChuteSimulator chute = new DeliveryChuteSimulator(10);
+		displayController.chuteFull(chute);
+		//assertEquals("Notice: Please remove all objects from chute and try again", basicOutStream.toString());
+				assertEquals("Notice: Please remove all objects from chute and try again", hardwareOutStream.toString());
+	}
 	
 	private DisplaySimulator display;
 	private DisplayController displayController;
-	private ByteArrayOutputStream outStream;
+	private ByteArrayOutputStream hardwareOutStream;
+	private ByteArrayOutputStream basicOutStream;
 
 }
